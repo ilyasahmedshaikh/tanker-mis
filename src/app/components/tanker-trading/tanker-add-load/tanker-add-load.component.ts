320da18202c889c871d9dde4192ff34ea45016e1
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2022-03-09 23:05:05 +0500

    tanker add load screen with pmg hsd cards ui done

--- src/app/components/tanker-trading/tanker-add-load/tanker-add-load.component.ts
+++ src/app/components/tanker-trading/tanker-add-load/tanker-add-load.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tanker-add-load',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TankerAddLoadComponent implements OnInit {
 
-  constructor() { }
+  programForm: any = FormGroup;
+  table: any = FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.formInit();
+  }
+
+  formInit() {
+    this.programForm = this.fb.group({
+      date: ['', Validators.required],
+      company: ['', Validators.required],
+      invoiceNo: ['', Validators.required],
+      vehicleNo: ['', Validators.required],
+      externalVehicleNo: ['', Validators.required],
+      expenseAmount: ['', Validators.required],
+      vehicleRent: ['', Validators.required],
+    });
+    
+    this.table = [
+      {
+        loadBreakdown: '8,000 (PMG)',
+        rate: '145',
+        total: '1,160,000'
+      },
+      {
+        loadBreakdown: '16,000 (HSD)',
+        rate: '140',
+        total: '2,240,000'
+      },
+    ]
   }
 
 }
