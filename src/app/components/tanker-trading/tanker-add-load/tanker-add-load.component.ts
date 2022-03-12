103d5beaeccc9ac38233bf7b19bcfc106869b7f6
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2022-03-12 16:26:21 +0500

    tanker release load screen ui and other file fixes done

--- src/app/components/tanker-trading/tanker-add-load/tanker-add-load.component.ts
+++ src/app/components/tanker-trading/tanker-add-load/tanker-add-load.component.ts
@@ -10,6 +10,10 @@ export class TankerAddLoadComponent implements OnInit {
 
   programForm: any = FormGroup;
   table: any = FormGroup;
+  companies: any = [];
+  vehicles: any = [];
+
+  isExternal: boolean = false;
 
   constructor(
     private fb: FormBuilder
@@ -17,6 +21,51 @@ export class TankerAddLoadComponent implements OnInit {
 
   ngOnInit(): void {
     this.formInit();
+
+    this.companies = [
+      {
+        id: 1,
+        company: 'PSO Petrol Pump'
+      },
+      {
+        id: 2,
+        company: 'ATTOCK Petrol Pump'
+      },
+      {
+        id: 3,
+        company: 'TOTAL PARCO Petrol Pump'
+      },
+      {
+        id: 4,
+        company: 'BYCO Petrol Pump'
+      },
+      {
+        id: 5,
+        company: 'HASCOL Petrol Pump'
+      },
+      {
+        id: 6,
+        company: 'SHELL Petrol Pump'
+      }
+    ]
+    this.vehicles = [
+      {
+        id: 1,
+        vehicleNo: 'JF-9001'
+      },
+      {
+        id: 2,
+        vehicleNo: 'KW-6001'
+      },
+      {
+        id: 3,
+        vehicleNo: 'JV-0801'
+      },
+      {
+        id: 4,
+        vehicleNo: 'PN-8976'
+      }
+    ]
   }
 
   formInit() {
@@ -27,7 +76,7 @@ export class TankerAddLoadComponent implements OnInit {
       vehicleNo: ['', Validators.required],
       externalVehicleNo: ['', Validators.required],
       expenseAmount: ['', Validators.required],
-      vehicleRent: ['', Validators.required],
+      vehicleRent: ['1.20', Validators.required],
     });
     
     this.table = [
@@ -42,6 +91,18 @@ export class TankerAddLoadComponent implements OnInit {
         total: '2,240,000'
       },
     ]
+  }
+
+  onExternal() {
+    this.isExternal = !this.isExternal;
+  }
+
+  checkIfExternal(event: any) {
+    console.log(event.target.value);
+    
+    if(event.target.value == 'external') {
+      this.onExternal();
+    }
   }
 
 }
