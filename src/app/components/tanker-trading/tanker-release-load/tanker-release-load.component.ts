3fba50dabdab45c75c3c0d8500e92dbd0ac18ef7
Muhammad Ilyas <46137314+ilyasahmedshaikh@users.noreply.github.com>
2022-03-12 16:27:20 +0500

    tanker release load screen ui and other file fixes done (#6)

--- src/app/components/tanker-trading/tanker-release-load/tanker-release-load.component.ts
+++ src/app/components/tanker-trading/tanker-release-load/tanker-release-load.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tanker-release-load',
@@ -7,9 +8,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TankerReleaseLoadComponent implements OnInit {
 
-  constructor() { }
+  programForm: any = FormGroup;
+  vehicles: any = [];
+
+  isExternal: boolean = false;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.formInit();
+
+    this.vehicles = [
+      {
+        id: 1,
+        vehicleNo: 'JF-9001'
+      },
+      {
+        id: 2,
+        vehicleNo: 'KW-6001'
+      },
+      {
+        id: 3,
+        vehicleNo: 'JV-0801'
+      },
+      {
+        id: 4,
+        vehicleNo: 'PN-8976'
+      }
+    ]
+  }
+
+  formInit() {
+    this.programForm = this.fb.group({
+      vehicleNo: ['', Validators.required],
+      externalVehicleNo: ['', Validators.required],
+      litresSell: ['', Validators.required],
+      typeSell: ['', Validators.required],
+      purchaseRate: ['145', Validators.required],
+      rentPerLitre: ['1.20', Validators.required],
+      perLitreSellPrice: ['', Validators.required],
+      commissionPerLitre: ['', Validators.required],
+      commissionTotal: ['', Validators.required],
+      soldAddress: ['', Validators.required],
+      otherDetails: ['', Validators.required],
+    });
+  }
+
+  onExternal() {
+    this.isExternal = !this.isExternal;
+  }
+
+  checkIfExternal(event: any) {
+    console.log(event.target.value);
+    
+    if(event.target.value == 'external') {
+      this.onExternal();
+    }
   }
 
 }
