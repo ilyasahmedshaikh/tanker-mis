844684442a83e404f5f0390949afc78fe3ae3eb2
Muhammad Ilyas <46137314+ilyasahmedshaikh@users.noreply.github.com>
2022-03-25 21:56:57 +0500

    dashboard screen ui done with responsive (#13)

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 
@@ -11,7 +13,9 @@ import { DashboardComponent } from './dashboard.component';
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule
   ]
 })
 export class DashboardModule { }
