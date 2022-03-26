1d82572d19d6ecae364bacf63f5dccbd99481645
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2022-03-26 21:12:57 +0500

    reports all ui screens done with responsive

--- src/app/components/reports/sales-tanker-report/sales-tanker-report.component.ts
+++ src/app/components/reports/sales-tanker-report/sales-tanker-report.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesTankerReportComponent implements OnInit {
 
+  table: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.table = [
+      {
+        id: 1,
+        noPlate: 'KU-4101',
+        nameType: '6 Wheeler | 10000 LTS.',
+      },
+      {
+        id: 2,
+        noPlate: 'KU-9999',
+        nameType: '6 Wheeler | 15000 LTS.',
+      },
+      {
+        id: 3,
+        noPlate: 'KW-6001',
+        nameType: '8 Wheeler | 5000 LTS.',
+      },
+      {
+        id: 4,
+        noPlate: 'JV-0801',
+        nameType: '6 Wheeler | 20000 LTS.',
+      },
+      {
+        id: 5,
+        noPlate: 'JF-9001',
+        nameType: '8 Wheeler | 15000 LTS.',
+      },
+      {
+        id: 6,
+        noPlate: 'JS-5501',
+        nameType: '4 Wheeler | 10000 LTS.',
+      },
+    ]
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SearchBarComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SearchBarComponent
   ]
 })
 export class SharedModule { }
